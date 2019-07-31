a72fc0244ca782b3e51d3bc7978eb6cd45ec5231
ellisonguimaraes <ellison.guimaraes@gmail.com>
2019-07-31 11:57:23 -0300

    Divisão das sessões

--- montador.py
+++ montador.py
@@ -20,28 +20,49 @@ def DeleteCommWhileLines(i):
     #Tirando comentários
     if(';' in i):
         i = i[:i.find(';')]
-    #Tirando linhas em branco
-    if(i):
-        return i
 
-    return None
+    return i
 
 
-Labels = []
-Instructions = []
+
+section_data = []
+section_bss = []
+section_text = []
+FileList = []
+
 
 file = open("portao.asm", 'r')
 
 
+#TRATANDO AS LINHAS: Retirando comentários, linhas em branco e tabulações e quebra de linha
 for i in file:
     i = DeleteCommWhileLines(i)
-
     if(i):
-        if("section .bss" in i):
-            print(i)
+        FileList.append(i)
+
+
+#SEPARANDO SESSÕES EM LISTAS DIFERENTES
+SectionIndexes = sorted(((FileList.index("section .bss"), "section .bss"),
+     (FileList.index("section .data"), "section .data"),
+     (FileList.index("section .text"), "section .text")))
+
+for i in range(len(SectionIndexes)):
+    l = FileList[SectionIndexes[i][0] : SectionIndexes[i+1][0] if i < len(SectionIndexes)-1 else len(FileList)]
+    if(SectionIndexes[i][1] == "section .data"):
+        section_data = l
+    elif(SectionIndexes[i][1] == "section .bss"):
+        section_bss = l
+    else:
+        section_text = l
 
 
 
+
+
+
+#SectionData = FileList[FileList.index("section .data"): FileList.index("section .bss")]
+#SectionBss = FileList[FileList.index("section .bss"): FileList.index("section .text")]
+#SectionText = FileList[FileList.index("section .text"):]
 
 
 
